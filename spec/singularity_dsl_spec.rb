3e49225123c462119ba5a231d86345992c68f88a
chr0n1x <heilong24@gmail.com>
2014-06-04 11:13:23 -0400

    makin tests better with @attamusc

--- spec/singularity_dsl_spec.rb
+++ spec/singularity_dsl_spec.rb
@@ -12,8 +12,8 @@ include SingularityDsl
 describe 'SingularityDsl' do
   context '#task_name' do
     it 'returns classname without context' do
-      expect(SingularityDsl.task_name('Class::Foo')).to eql('Foo')
-      expect(SingularityDsl.task_name('Foo')).to eql('Foo')
+      expect(SingularityDsl.task_name('Class::Foo')).to eql 'Foo'
+      expect(SingularityDsl.task_name('Foo')).to eql 'Foo'
     end
   end
 
@@ -27,7 +27,7 @@ describe 'SingularityDsl' do
   context '#task_list' do
     it 'returns an array of Tasks' do
       tasks = SingularityDsl.task_list
-      expect(tasks).to be_a_kind_of(Array)
+      expect(tasks).to be_a_kind_of Array
       tasks.each do |task|
         task.should be < Task
       end
@@ -36,11 +36,8 @@ describe 'SingularityDsl' do
 
   context '#load_tasks' do
     it 'dynamically creates task methods' do
-      context_it = self
       SingularityDsl.load_tasks
-      TestTask do
-        context_it.expect(true).to context_it.eql(true)
-      end
+      expect(SingularityDsl.method_defined? :TestTask).to eql true
     end
   end
 end
