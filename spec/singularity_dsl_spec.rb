b7ec5355f069519198c1dfa4b0d99695b36e4d03
chr0n1x <heilong24@gmail.com>
2014-06-04 10:21:30 -0400

    singularity_dsl_spec: covering all methods

--- spec/singularity_dsl_spec.rb
+++ spec/singularity_dsl_spec.rb
@@ -2,12 +2,45 @@
 
 require 'singularity_dsl'
 
+module SingularityDsl
+  class TestTask < Task
+  end
+end
+
 include SingularityDsl
 
 describe 'SingularityDsl' do
   context '#task_name' do
     it 'returns classname without context' do
-      expect(SingularityDsl.task_name('Some::Class::Foo')).to eql('Foo')
+      expect(SingularityDsl.task_name('Class::Foo')).to eql('Foo')
+      expect(SingularityDsl.task_name('Foo')).to eql('Foo')
+    end
+  end
+
+  context '#task_to_sym' do
+    it 'returns symbol' do
+      sym = SingularityDsl.task_to_sym('Some::Class::Foo')
+      expect { throw sym }.to throw_symbol
+    end
+  end
+
+  context '#task_list' do
+    it 'returns an array of Tasks' do
+      tasks = SingularityDsl.task_list
+      expect(tasks).to be_a_kind_of(Array)
+      tasks.each do |task|
+        task.should be < Task
+      end
+    end
+  end
+
+  context '#load_tasks' do
+    it 'dynamically creates task methods' do
+      context_it = self
+      SingularityDsl.load_tasks
+      TestTask do
+        context_it.expect(true).to context_it.eql(true)
+      end
     end
   end
 end
