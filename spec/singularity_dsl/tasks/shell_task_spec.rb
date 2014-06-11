9666e16d07d5a9b16e75ea038ac95e9928b690fb
chr0n1x <heilong24@gmail.com>
2014-06-11 15:43:55 -0400

    [feature] first implementation of shell_task

---
+++ spec/singularity_dsl/tasks/shell_task_spec.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+require 'singularity_dsl/tasks/shell_task'
+
+describe 'ShellTask' do
+  let(:sh_task) { SingularityDsl::ShellTask.new }
+
+  context '#command' do
+    it 'sets commands!' do
+      cmd = 'echo "hi :)"'
+      sh_task.command cmd
+      expect(sh_task.shell.command).to eql cmd
+    end
+  end
+
+  context '#execute' do
+    it 'errors when command() was never called' do
+      expect { sh_task.execute }.to raise_error
+    end
+
+    it 'runs command returns correct status' do
+      cmd = 'echo "hi :)"'
+      sh_task.command cmd
+      expect(sh_task.shell).to receive(:run_command)
+      expect(sh_task.shell).to receive(:exitstatus).and_return 0
+      expect(sh_task.execute).to eql 0
+    end
+  end
+end
