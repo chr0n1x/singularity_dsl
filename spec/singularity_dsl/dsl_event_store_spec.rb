a39528874a255ca202eb2bae84c791b5f4a8e968
chr0n1x <heilong24@gmail.com>
2014-06-16 09:57:56 -0400

    [refactor] updating dsl_event_store spec

--- spec/singularity_dsl/dsl_event_store_spec.rb
+++ spec/singularity_dsl/dsl_event_store_spec.rb
@@ -1,41 +1,44 @@
 # encoding: utf-8
 
-require 'singularity_dsl/dsl_defaults'
+require 'singularity_dsl/dsl_event_store'
 
-describe 'DslDefaults' do
-  before :each do
-    @instance = SingularityDsl::DslDefaults.new
-  end
+# simple container to include DslEventStore
+class EventStoreTest
+  include SingularityDsl::DslEventStore
+end
+
+describe 'DslEventStore' do
+  let(:instance) { EventStoreTest.new }
 
   context '#on_error' do
     it 'creates an error_proc' do
-      expect(@instance.error_proc).to be_a_kind_of Proc
-      @instance.on_error {}
-      expect(@instance.error_proc).to be_a_kind_of Proc
+      expect(instance.error_proc).to be_a_kind_of Proc
+      instance.on_error {}
+      expect(instance.error_proc).to be_a_kind_of Proc
     end
   end
 
   context '#on_fail' do
     it 'creates an fail_proc' do
-      expect(@instance.fail_proc).to be_a_kind_of Proc
-      @instance.on_fail {}
-      expect(@instance.fail_proc).to be_a_kind_of Proc
+      expect(instance.fail_proc).to be_a_kind_of Proc
+      instance.on_fail {}
+      expect(instance.fail_proc).to be_a_kind_of Proc
     end
   end
 
   context '#on_success' do
     it 'creates an error_proc' do
-      expect(@instance.success_proc).to be_a_kind_of Proc
-      @instance.on_success {}
-      expect(@instance.success_proc).to be_a_kind_of Proc
+      expect(instance.success_proc).to be_a_kind_of Proc
+      instance.on_success {}
+      expect(instance.success_proc).to be_a_kind_of Proc
     end
   end
 
   context '#always' do
     it 'creates an error_proc' do
-      expect(@instance.always_proc).to be_a_kind_of Proc
-      @instance.always {}
-      expect(@instance.always_proc).to be_a_kind_of Proc
+      expect(instance.always_proc).to be_a_kind_of Proc
+      instance.always {}
+      expect(instance.always_proc).to be_a_kind_of Proc
     end
   end
 end
