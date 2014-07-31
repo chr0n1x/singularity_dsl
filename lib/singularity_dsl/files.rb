281cbfe1f60682ab93551f82ab109b8c34591096
chr0n1x <heilong24@gmail.com>
2014-07-31 15:06:57 -0400

    DSL: only load .rb files

--- lib/singularity_dsl/files.rb
+++ lib/singularity_dsl/files.rb
@@ -5,16 +5,21 @@ module SingularityDsl
   module Files
     private
 
-    def files_in_path(path)
+    def files_in_path(path, extension = false)
       paths = [path] if ::File.file? path
       paths = dir_glob path if ::File.directory? path
       paths ||= []
+      paths = filter_ext paths, extension if extension
       paths
     end
 
     def dir_glob(dir)
       dir = ::File.join dir, '**'
       ::Dir.glob dir
+    end
+
+    def filter_ext(path_list, ext)
+      path_list.select { |file| file.match(/\.#{ext}$/) }
     end
   end
 end
