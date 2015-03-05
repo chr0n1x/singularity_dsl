545788996211f0a8ef39c6e88f4e49459c31bf96
chr0n1x <heilong24@gmail.com>
2015-03-05 18:42:49 -0500

    git_helper: actually return array

--- lib/singularity_dsl/application.rb
+++ lib/singularity_dsl/application.rb
@@ -45,7 +45,6 @@ module SingularityDsl
     end
 
     def change_list(list)
-      list = list.split("\n") if list.is_a? String
       list = [*list]
       list.sort!
       @runner.dsl.changeset = list
